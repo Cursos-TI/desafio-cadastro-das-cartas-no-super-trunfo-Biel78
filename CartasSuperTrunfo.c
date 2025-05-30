9bcf333b55322694f75bb6f6d8d4565fdfdcdc30
Biel78 <125318111+Biel78@users.noreply.github.com>
2025-05-30 20:58:55 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
 
+int main() {
+
+char estado1;
+char codigo1[4];
+char nomeCidade1[50];
+int populacao1;
+float area1;
+float pib1;
+int PontosTuristicos1;
+
+char estado2;
+char codigo2[4];
+char nomeCidade2[50];
+int populacao2;
+float area2;
+float pib2;
+int PontosTuristicos2;
+
 // Desafio Super Trunfo - Países
 // Tema 1 - Cadastro das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
